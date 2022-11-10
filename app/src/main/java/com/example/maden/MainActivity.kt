9c4517fd1ec0694aade6cd076020991775d7a53b
fmaden <f.maden.117@hotmail.com>
2022-11-11 01:22:15 +0200

    maden

--- app/src/main/java/com/example/maden/MainActivity.kt
+++ app/src/main/java/com/example/maden/MainActivity.kt
@@ -20,9 +20,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 
-/**
- * Activity for cupcake order flow.
- */
+
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
